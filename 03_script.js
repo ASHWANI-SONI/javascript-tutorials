8c1ed7ca246c1cbce40fdda7e340ab29d544ec1d
ashwani072 <97818686+ashwani072@users.noreply.github.com>
2023-05-06 13:33:43 +0530

    uploaded tutorials

---
+++ 03_script.js
@@ -0,0 +1,7 @@
+document.getElementById("demo").innerHTML = 5 + 6;
+document.write(5 + 7) //  for testing purpose use this to write
+function boom(){
+    document.write(7+7);
+    window.print();
+}
+// window.alert('hello world');
